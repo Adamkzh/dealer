3d2d0a52048e22de1e0e3afa5d04b9f0290a4011
siriustom <linxiaoran0210@gmail.com>
2018-11-13 21:43:31 -0800

    add and modify config file

--- utils/mySQLUtils.js
+++ utils/mySQLUtils.js
@@ -1,19 +1,19 @@
-var config = require('config');
-var mysql = require('mysql');
-var pool = mysql.createPool({
+let config = require('config');
+let mysql = require('mysql');
+let pool = mysql.createPool({
   connectionLimit : config.get('dbConfig.connectionLimit'),
   host            : config.get('dbConfig.host'),
   user            : config.get('dbConfig.user'),
   password        : config.get('dbConfig.password'),
-  database        : config.get('dbConfig.user')
+  database        : config.get('dbConfig.user'),
 });
 
-module.exports.query = function(sql, options, callback){  
-    pool.getConnection(function(err, conn){  
-        if(err){  
+module.exports.query = function(sql, options, callback) {
+    pool.getConnection(function(err, conn) {
+        if (err) {
             callback(err, null, null);  
-        }else{  
-            conn.query(sql, options, function(err, results, fields){
+        } else {
+            conn.query(sql, options, function(err, results, fields) {
                 // release connection
                 conn.release(); 
                 callback(err, results, fields); 
@@ -22,21 +22,19 @@ module.exports.query = function(sql, options, callback){
     });  
 };
 
-module.exports.getDBPool = function () {
+module.exports.getDBPool = function() {
     return pool;
-}
+};
 
 
 module.exports.handleError = function(err) {
-	if(err)
-	{
+	if(err) {
 	 	console.log(err);
 	  	throw err;
 	}
-}
+};
 
 
-module.exports.getDBName = function()
-{
+module.exports.getDBName = function() {
 	return config.get('dbConfig.user');
-}
+};
