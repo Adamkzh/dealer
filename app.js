95450f45601e983fd82de61281f678a90edb019d
siriustom <linxiaoran0210@gmail.com>
2018-11-14 17:26:22 -0800

    fix db connection bg

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
 
-let mysql = require('mysql');
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
