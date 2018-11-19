1c501c1e34038d554b0834f7a8cbbfca4197a23e
bohan <bohan612@gmail.com>
2018-11-19 00:17:48 -0800

    modified home page

--- services/cars-services.js
+++ services/cars-services.js
@@ -37,6 +37,23 @@ module.exports.getCarById = function(carId) {
     });
 };
 
+module.exports.getAllCars = function() {
+    return new Promise((resolve, reject) => {
+        let queryStr =
+            'select * from car ';
+        dbUtil.query(queryStr, [], function(err, result, fields) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            if (result.length === 0) {
+                result.push({});
+            }
+            resolve(JSON.parse(JSON.stringify(result[0])));
+        });
+    });
+};
+
 module.exports.addCar = function(manufacture, model, year, owner) {
     return new Promise((resolve, reject) => {
         let queryStr =
