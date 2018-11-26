33f9a071f2bae212c04a842e7084b989cd4f6126
siriustom <linxiaoran0210@gmail.com>
2018-11-26 11:19:39 -0800

    add post car GET and POST controller

--- controller/ctl-car.js
+++ controller/ctl-car.js
@@ -9,3 +9,14 @@ module.exports.getSearch = function(req, res, next) {
             console.log(err);
         });
 };
+
+module.exports.postCar = function(req, res, next) {
+    let {manufacture, model, year, owner} = {...req.body};
+    carServices.addCar(manufacture, model, year, owner)
+        .then((result) => {
+            console.log(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
